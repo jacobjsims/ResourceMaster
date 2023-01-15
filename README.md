742c929085b575cd49f0eb0e0c2f2e37664ed420
Jacob Sims <Jsims44@gmail.com>
2023-01-15 09:17:39 -0600

    Initial fleshout

---
+++ README.md
@@ -0,0 +1,5 @@
+# Resource Master
+
+## Overview
+Resource master provides a robust set of APIs for managing a collection of resources and the commitments associated with them. It was born out of repeat challenges that arise during budget and quoting activities for software engineering. 
+
--- main.go
+++ main.go
@@ -1,17 +1,38 @@
 package main
 
 import (
-	"net/http"
-
+	util "ResourceMaster/utils"
 	"github.com/gin-gonic/gin"
+	"net/http"
 )
 
 func main() {
+	// Set up the server
+	router := SetupRouter()
+
 	r := gin.Default()
 	r.GET("/ping", func(c *gin.Context) {
 		c.JSON(http.StatusOK, gin.H{
 			"message": "pong",
 		})
 	})
+
 	r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
+}
+
+func SetupRouter() *gin.Engine {
+
+	// Set up the router
+	router := gin.Default()
+
+	// Set up the application mode
+	if util.GodotEnv("GO_ENV") != "production" && util.GodotEnv("GO_ENV") != "test" {
+		gin.SetMode(gin.DebugMode)
+	} else if util.GodotEnv("GO_ENV") == "test" {
+		gin.SetMode(gin.TestMode)
+	} else {
+		gin.SetMode(gin.ReleaseMode)
+	}
+
+	return router
 }
